a4c81208a6114ca2969907a708f2e2e43cc78949
wxs <wxs_zzu@163.com>
2019-05-28 10:38:21 +0800

    创建一个dubbo项目

---
+++ src/main/java/com/wxs/api/WxsApiApplication.java
@@ -0,0 +1,17 @@
+package com.wxs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wxs
+ * @data
+ */
+@SpringBootApplication
+public class WxsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxsApiApplication.class, args);
+    }
+
+}
