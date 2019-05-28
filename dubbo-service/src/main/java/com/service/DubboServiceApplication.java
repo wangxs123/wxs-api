3c5278c07c5c30ae3715708b8a759bdcf6bb7577
wxs <wxs_zzu@163.com>
2019-05-28 15:14:12 +0800

    相关修改

--- dubbo-service/src/main/java/com/service/DubboServiceApplication.java
+++ dubbo-service/src/main/java/com/service/DubboServiceApplication.java
@@ -1,17 +1,17 @@
-package com.wxs.api;
+package com.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author wxs
- * @data 2019-05-28 10:00
+ *
  */
 @SpringBootApplication
-public class WxsApiApplication {
+public class DubboServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WxsApiApplication.class, args);
+        SpringApplication.run(DubboServiceApplication.class, args);
     }
 
 }
--- dubbo-service/src/test/java/com/service/DubboServiceApplicationTests.java
+++ dubbo-service/src/test/java/com/service/DubboServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wxs.api;
+package com.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WxsApiApplicationTests {
+public class DubboServiceApplicationTests {
 
     @Test
     public void contextLoads() {
