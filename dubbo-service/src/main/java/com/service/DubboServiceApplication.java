e3e35dd7c2ce683a37f45aa4c2c044cd7b312ed2
wxs <wxs_zzu@163.com>
2019-05-29 17:15:32 +0800

    基础配置信息

--- dubbo-service/src/main/java/com/service/DubboServiceApplication.java
+++ dubbo-service/src/main/java/com/service/DubboServiceApplication.java
@@ -1,5 +1,7 @@
 package com.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DubboServiceApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(DubboServiceApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DubboServiceApplication.class, args);
+        logger.info("wxs-api服务启动成功");
     }
 
 }
